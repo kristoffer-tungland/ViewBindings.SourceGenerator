690a6cc68c2dca319563b3a079d2bb8ddd707c5c
Kristoffer Tungland <ktu@cowi.com>
2023-01-03 08:45:07 +0100

    Updated version

--- ViewBindings.SourceGenerator.Demo/Views/SecondView.xaml.cs
+++ ViewBindings.SourceGenerator.Demo/Views/SecondView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace ViewBindings.SourceGenerator.Demo.Views
+namespace ViewBindings.SourceGenerator.Demo.Views;
+
+/// <summary>
+/// Interaction logic for View2.xaml
+/// </summary>
+public partial class SecondView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for View2.xaml
-    /// </summary>
-    public partial class SecondView : UserControl
+    public SecondView()
     {
-        public SecondView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
